4ba6c21806ac05f1144cb6d97b6bf2609e2d34f6
fabiooo000 <fabio.ventura.0602s@gmail.com>
2025-08-26 22:55:16 -0500

    Create S3s1.py

---
+++ S3s1.py
@@ -0,0 +1,8 @@
+#SQL Server es un sistema de gestión de bases de datos
+#relacional utilizado para almacenar y administrar datos,
+#mientras que SSMS es una herramienta que proporciona una
+#interfaz gráfica para administrar y trabajar con bases de
+#datos SQL Server.
+#El modelo E-R, es un modelo conceptual, no se implementa,
+#sirve para plantear entidades, relaciones y atributos que a
+#futuro se convierten en tablas en un modelo lógico.
